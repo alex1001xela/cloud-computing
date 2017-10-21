4b05e5f00f85715a0c9ec0f9c90af45388452cda
Alex <alexandros.konstantakos@student.reutlingen-university.de>
2017-10-21 12:26:37 +0200

    Auto stash before merge of "master" and "origin/master"

--- src/client/scripts/chat.js
+++ src/client/scripts/chat.js
@@ -1,46 +1,17 @@
 var socket = io();
 function Chat() {
 
-	var username = "";
 	var domElement = document.createElement("DIV");
 	document.body.appendChild(domElement);
 
-	//var loginScreen = new LoginScreen(domElement);
-
 	new ChatScreen(domElement);
 
-	/*
-	loginScreen.onLogin(function () {
 
-		new ChatScreen();
-    });
-	*/
+	/*var loginScreen = new LoginScreen(domElement);
+	loginScreen.onLogin(function (username) {
+		loginScreen.detach();
+		loginScreen = null;
+		// new ChatScreen(parent, username);
+    });*/
 
-	/* BEISPIEL
-
-	var randomDiv = document.createElement("DIV");
-	randomDiv.textContent = "Connected!";
-
-	var randomButton = document.createElement("BUTTON");
-	randomButton.textContent = "Press me!";
-	randomButton.onclick = function () {
-		domElement.removeChild(randomDiv);
-		domElement.removeChild(randomButton);
-
-		var randomDiv2 = document.createElement("DIV");
-		randomDiv2.textContent = "Well done!";
-		domElement.appendChild(randomDiv2);
-	};
-
-
-	socket.on("connect", function () {
-		domElement.appendChild(randomDiv);
-		domElement.appendChild(randomButton);
-	});
-
-	// BEISPIEL ENDE */
-
-	function showChatScreen() {
-
-	}
 }
--- src/client/scripts/loginScreen.js
+++ src/client/scripts/loginScreen.js
@@ -1,32 +1,50 @@
 function LoginScreen(parent){
     var loginScreen = {};
-
-    var domElement = document.createElement("DIV");
-    var usernameInputField = document.createElement("INPUT");
-    var submitButton = document.createElement("BUTTON");
     var onLoginCallback;
+    var domElement = document.createElement("DIV");
+    domElement.className = "login-screen";
+    var usernameInputField = document.createElement("INPUT");
+    usernameInputField.className = "username-input-field";
+    var submitButton = document.createElement("BUTTON");
+    submitButton.className = "submit-login-button";
+    submitButton.textContent = "Submit";
+
+    domElement.appendChild(usernameInputField);
+    domElement.appendChild(submitButton);
 
     parent.appendChild(domElement);
 
 
     submitButton.onclick = function () {
-        submitUsername();
+        if(usernameInputField.value.length > 0){
+            submitUsername(usernameInputField.value);
+        }
+    };
+
+    usernameInputField.onkeyup = function (event) {
+        if(event.key === "Enter" && usernameInputField.value.length > 0){
+            submitUsername(usernameInputField.value);
+        }
     };
 
 
-    loginScreen.submitUsername = function(username) {
+    function submitUsername (username) {
         socket.emit("login", username, function (loginSuccessful) {
             if(loginSuccessful){
                 onLoginCallback();
             }
             else{
-
+                alert("This username already exists!");
             }
         });
-    };
+    }
 
     loginScreen.onLogin = function(callback) {
         onLoginCallback = callback;
+    };
+
+    loginScreen.detach = function () {
+        parent.removeChild(domElement);
     };
 
     return loginScreen;
