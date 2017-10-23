4167e3ee389a371549b8715ff638c0acdea4fbc4
zahidyusuf <zahid_ibnu.yusuf@student.reutlingen-university.de>
2017-10-23 16:31:30 +0200

    Changes chat.js, loginScreen.js und CSS
    Changes chat.js, loginScreen.js und CSS

--- src/client/scripts/chat.js
+++ src/client/scripts/chat.js
@@ -1,19 +1,25 @@
 var socket = io();
 function Chat() {
 
+	/*
 	var domElement = document.createElement("DIV");
 	document.body.appendChild(domElement);
 	socket.emit("login", "Alex", function () {
 
     });
+
 	new ChatScreen(domElement);
-	
+	*/
+
 	//new MessageEditor(domElement);
 
-	/*var loginScreen = new LoginScreen(domElement);
+	var domElement = document.createElement("DIV");
+	document.body.appendChild(domElement);
+	var loginScreen = new LoginScreen(domElement);
 	loginScreen.onLogin(function (username) {
 		loginScreen.detach();
 		loginScreen = null;
-		// new ChatScreen(parent, username);
-    });*/
+		new ChatScreen(domElement, username);
+    });
+    
 }
