14bb97fa90a38cd7d074f6d365e393515f60f73a
Alex <alexandros.konstantakos@student.reutlingen-university.de>
2017-11-17 09:37:23 +0100

    Put some register screen

--- client/dist/bundle.js
+++ client/dist/bundle.js
@@ -7250,6 +7250,9 @@ function Attachment(fileBuffer, type, name, size) {
 
 "use strict";
 /* harmony export (immutable) */ __webpack_exports__["a"] = LoginScreen;
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__registerScreen__ = __webpack_require__(49);
+
+
 function LoginScreen(parent, socket){
     var loginScreen = {};
     var onLoginCallback;
@@ -7268,8 +7271,6 @@ function LoginScreen(parent, socket){
     paragraph.className = "paragraph";
     domElement.appendChild(paragraph);
 
-
-
     var usernameInputField = document.createElement("INPUT");
     usernameInputField.className = "username-input-field";
     
@@ -7281,9 +7282,10 @@ function LoginScreen(parent, socket){
 
     domElement.appendChild(usernameInputField);
 
+
     var submitButton = document.createElement("BUTTON");
     submitButton.className = "submit-login-button";
-    submitButton.textContent = "SUBMIT";
+    submitButton.textContent = "LOGIN";
     
     submitButton.onclick = function () {
         if(usernameInputField.value.length > 0){
@@ -7291,6 +7293,17 @@ function LoginScreen(parent, socket){
         }
     };
     domElement.appendChild(submitButton);
+
+    var registerScreenLink = document.createElement("DIV");
+    registerScreenLink.className = "register-screen-link";
+    registerScreenLink.textContent = "Not registered? Press this to register";
+
+    registerScreenLink.onclick = function () {
+        loginScreen.detach();
+        new __WEBPACK_IMPORTED_MODULE_0__registerScreen__["a" /* default */](parent, socket);
+    };
+
+    domElement.appendChild(registerScreenLink);
 
     parent.appendChild(domElement);
     usernameInputField.focus();
@@ -7326,6 +7339,96 @@ function LoginScreen(parent, socket){
     };
 
     return loginScreen;
+}
+
+/***/ }),
+/* 49 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (immutable) */ __webpack_exports__["a"] = RegisterScreen;
+function RegisterScreen(parent, socket) {
+    var registerScreen = {};
+    var domElement = document.createElement("DIV");
+    domElement.className = "register-screen";
+
+    var logo = document.createElement("IMG");
+    logo.setAttribute("src","resources/logo.jpg");
+    logo.setAttribute("width", "436");
+    logo.setAttribute("height", "116");
+    domElement.appendChild(logo);
+    logo.className = "logo";
+
+    var paragraph = document.createElement("P");
+    paragraph.textContent = "Please enter your name!";
+    paragraph.className = "paragraph";
+    domElement.appendChild(paragraph);
+
+    var usernameInputField = document.createElement("INPUT");
+    usernameInputField.className = "username-input-field";
+
+    usernameInputField.onkeyup = function (event) {
+        if(event.key === "Enter" && usernameInputField.value.length > 0){
+            submitUsername(usernameInputField.value);
+        }
+    };
+
+    var registerScreenLink = document.createElement("DIV");
+    registerScreenLink.className = "register-screen-link";
+    registerScreenLink.textContent = "Not registered? Press this to register";
+
+    registerScreenLink.onclick = function () {
+        registerScreen.detach();
+        new RegisterScreen(parent, socket);
+    };
+
+    domElement.appendChild(usernameInputField);
+
+    var submitButton = document.createElement("BUTTON");
+    submitButton.className = "submit-login-button";
+    submitButton.textContent = "REGISTER";
+
+    submitButton.onclick = function () {
+        if(usernameInputField.value.length > 0){
+            submitUsername(usernameInputField.value);
+        }
+    };
+    domElement.appendChild(submitButton);
+
+    parent.appendChild(domElement);
+    usernameInputField.focus();
+
+    function submitUsername (username) {
+
+        if(isUsernameValid(username.trim())){
+            socket.emit("login", username, function (loginSuccessful) {
+                if(loginSuccessful.status){
+                    onLoginCallback();
+                }
+                else{
+                    alert(loginSuccessful.reason);
+                }
+            });
+        }
+        else{
+            alert("Please enter a username without an empty space.");
+        }
+
+    }
+
+    function isUsernameValid(username) {
+        return !username.includes(" ");
+    }
+
+    registerScreen.onLogin = function(callback) {
+        onLoginCallback = callback;
+    };
+
+    registerScreen.detach = function () {
+        parent.removeChild(domElement);
+    };
+
+    return registerScreen;
 }
 
 /***/ })
--- client/scripts/registerScreen.js
+++ client/scripts/registerScreen.js
@@ -1,11 +1,8 @@
-import RegisterScreen from "./registerScreen";
-
-export default function LoginScreen(parent, socket){
-    var loginScreen = {};
-    var onLoginCallback;
+export default function RegisterScreen(parent, socket) {
+    var registerScreen = {};
     var domElement = document.createElement("DIV");
-    domElement.className = "login-screen";
-    
+    domElement.className = "register-screen";
+
     var logo = document.createElement("IMG");
     logo.setAttribute("src","resources/logo.jpg");
     logo.setAttribute("width", "436");
@@ -20,20 +17,28 @@ export default function LoginScreen(parent, socket){
 
     var usernameInputField = document.createElement("INPUT");
     usernameInputField.className = "username-input-field";
-    
+
     usernameInputField.onkeyup = function (event) {
         if(event.key === "Enter" && usernameInputField.value.length > 0){
             submitUsername(usernameInputField.value);
         }
     };
 
-    domElement.appendChild(usernameInputField);
+    var registerScreenLink = document.createElement("DIV");
+    registerScreenLink.className = "register-screen-link";
+    registerScreenLink.textContent = "Not registered? Press this to register";
 
+    registerScreenLink.onclick = function () {
+        registerScreen.detach();
+        new RegisterScreen(parent, socket);
+    };
+
+    domElement.appendChild(usernameInputField);
 
     var submitButton = document.createElement("BUTTON");
     submitButton.className = "submit-login-button";
-    submitButton.textContent = "LOGIN";
-    
+    submitButton.textContent = "REGISTER";
+
     submitButton.onclick = function () {
         if(usernameInputField.value.length > 0){
             submitUsername(usernameInputField.value);
@@ -41,49 +46,38 @@ export default function LoginScreen(parent, socket){
     };
     domElement.appendChild(submitButton);
 
-    var registerScreenLink = document.createElement("DIV");
-    registerScreenLink.className = "register-screen-link";
-    registerScreenLink.textContent = "Not registered? Press this to register";
-
-    registerScreenLink.onclick = function () {
-        loginScreen.detach();
-        new RegisterScreen(parent, socket);
-    };
-
-    domElement.appendChild(registerScreenLink);
-
     parent.appendChild(domElement);
     usernameInputField.focus();
 
     function submitUsername (username) {
 
         if(isUsernameValid(username.trim())){
-			socket.emit("login", username, function (loginSuccessful) {
-				if(loginSuccessful.status){
-					onLoginCallback();
-				}
-				else{
-					alert(loginSuccessful.reason);
-				}
-			});
+            socket.emit("login", username, function (loginSuccessful) {
+                if(loginSuccessful.status){
+                    onLoginCallback();
+                }
+                else{
+                    alert(loginSuccessful.reason);
+                }
+            });
         }
         else{
-			alert("Please enter a username without an empty space.");
+            alert("Please enter a username without an empty space.");
         }
 
     }
 
     function isUsernameValid(username) {
         return !username.includes(" ");
-	}
+    }
 
-    loginScreen.onLogin = function(callback) {
+    registerScreen.onLogin = function(callback) {
         onLoginCallback = callback;
     };
 
-    loginScreen.detach = function () {
+    registerScreen.detach = function () {
         parent.removeChild(domElement);
     };
 
-    return loginScreen;
+    return registerScreen;
 }
