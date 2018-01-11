a41f4f08702a84981478686baebdc6e57ff7b4d5
alexandrosntak <alexandros.konstantakos@student.reutlingen-university.de>
2018-01-11 17:29:58 +0100

    Starting from the beginning

--- server/app.js
+++ server/app.js
@@ -1,153 +1,34 @@
 "use strict";
-require("dotenv").config({silent: true});
 const express = require("express");
 const path = require("path");
 const initServer = require("./server");
-const SocketGateway = require("./socketGateway");
-const FileManager = require("./fileManager");
-const bodyParser = require("body-parser");
-const helmet = require("helmet");
-const socketIO = require("socket.io");
-const DatabaseManager = require("./databaseManager");
-const PictureAnalyzer = require("./pictureAnalyzer");
-
-
 
 const port = process.env.PORT || process.env.VCAP_APP_PORT || process.argv[2] || 8080;
 const homePath = path.join(__dirname, "..");
 const clientPath = path.join(homePath, "client");
-const profilePicturesPath = path.join(clientPath, "profilepictures");
-const tempPicturesPath = path.join(clientPath, "temppictures");
-
-const maxNumberOfUsers = 100;
 
 function App() {
 
 	this.expressApp = express();
-	this.expressApp.use(helmet());
-	this.expressApp.enable("trust proxy");
 
-    this.expressApp.use((req, res, next) => {
+	this.expressApp.use((req, res, next) => {
 		res.setHeader("Content-Security-Policy", "script-src 'self' " + "https://" + req.headers.host + req.url);
 
-        if (req.secure || req.headers.host === "localhost:" + port) { // allowing localhost without https
+		if (req.secure || req.headers.host === "localhost:" + port) { // allowing localhost without https
 
-            next();
-        } else {
-            res.redirect("https://" + req.headers.host + req.url);
-        }
+			next();
+		} else {
+			res.redirect("https://" + req.headers.host + req.url);
+		}
 
-    });
+	});
 
 	this.expressApp.use(express.static(clientPath));
-	this.expressApp.use(bodyParser.json());
 
 
 	this.users = {};
-	this.fileManager = new FileManager();
-	this.databaseManager = new DatabaseManager();
-	this.pictureAnalyzer = new PictureAnalyzer();
-	this.io = socketIO(initServer(port, this.expressApp, process.env.VCAP_APP_PORT));
 
-	setInterval(() => {
-		this.fileManager.deleteUploadsOlderThan(this.getOldestUserTimestamp());// purge some uploads every 30'
-	}, 30 * 60 * 1000);
-
-	new SocketGateway(this);
+	initServer(port, this.expressApp, process.env.VCAP_APP_PORT);
 }
-
-/*
-    Add the user
-*/
-App.prototype.addUser = function(username, socket) {
-
-	let loginSuccessful = {
-		status: false,
-		reason: ""
-	};
-
-	if(Object.keys(this.users).length === maxNumberOfUsers){
-		loginSuccessful.reason = "The server is full";
-	}
-	else {
-		this.users[username] = socket;
-		loginSuccessful.status = true;
-	}
-	return loginSuccessful;
-};
-
-/*
-    Remove the user
-*/
-App.prototype.removeUser = function (username) {
-	delete this.users[username];
-};
-
-/*
-Of all the users currently online, it gets the timestamp of the one who logged in first
- */
-App.prototype.getOldestUserTimestamp = function () {
-	let oldestTimestamp = Date.now();
-	for(let key in this.users){
-		if(this.users.hasOwnProperty(key)){
-			let userSocket = this.users[key];
-			let userTimestamp = userSocket.handshake.issued;
-			if(oldestTimestamp === undefined || userTimestamp < oldestTimestamp){
-				oldestTimestamp = userTimestamp;
-			}
-		}
-	}
-	return oldestTimestamp;
-};
-
-/*
-    Check if the username already exist on databases
-*/
-App.prototype.doesUsernameExist = function (username, callback) {
-	this.databaseManager.doesUsernameExist(username, callback);
-};
-
-/* 
-    Save the user data to database
-*/
-App.prototype.registerUser = function (registerData, callback) {
-	this.databaseManager.registerUser(registerData, callback);
-};
-
-/*
-Saves the profile picture temporarily and checks with the IBM picture analyzer if a face is present
- */
-App.prototype.isProfilePictureValid = function (pictureData, callback) {
-	this.fileManager.saveTemporaryProfilePicture(pictureData, (picturePath) => {
-
-		this.pictureAnalyzer.isFaceContainedInPicture(picturePath, (result) => {
-
-			callback(result);
-
-		});
-
-
-	});
-
-};
-
-/*
-    Check if the login data valid (username & password)
-*/
-App.prototype.areLoginDataValid = function (loginData, callback) {
-    this.databaseManager.areLoginDataValid(loginData, callback);
-};
-
-/*
-Moves the profile picture from the temppictures folder to the profilepictures folder
- */
-App.prototype.moveProfilePicture = function (picturePath, callback) {
-	let arrayPicturePathParts = picturePath.split("/");
-	let picture = arrayPicturePathParts[arrayPicturePathParts.length - 1];
-
-	this.fileManager.moveFile(path.join(tempPicturesPath, picture), path.join(profilePicturesPath, picture), () => {
-		callback(path.join("/", "profilepictures", picture));
-	});
-};
 
 new App();
